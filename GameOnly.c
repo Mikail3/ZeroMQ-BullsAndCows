39cace745536b263eab10be73da4229baef377ab
Mikail Cinar <43419941+Mikail3@users.noreply.github.com>
2021-05-02 18:52:24 +0200

    Update GameOnly.c
    FIxed the winning message

--- GameOnly.c
+++ GameOnly.c
@@ -1,77 +1,95 @@
-/* BULLS N COWS */
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-using namespace std;
-void splitNumber(int number, int array[], int size) {
-	for (; number != 0; number /= 10)
-		array[--size] = number%10;
-}
-main() {
-	srand(time(NULL));
-	
-	// prepare the game
-	cout << "--- welcome to BULLS & COWS game ---\n\n";
-	int number[100] = {0};
-	int size = 0;	// number of number that the user have guessed
-	// random the result
-	const short digit = 4;
-	int result = rand()%9000 + 1000;
-	// cout << result << endl;	// uncomment to TEST THE RESULT
-	int resultArr[digit];
-	splitNumber(result, resultArr, digit);	// split result to result in array
+#include <string.h>
 
-	// loop until the user guesses right
-	while(1) {
-		// input a number to guess
-		cout << "input a " << digit << "-digits number : ";
-		cin >> number[size];
-		if (number[size] < 1000 || number[size] > 9999) {
-			cout << "!!! " << digit << "-DIGITS NUMBER PLEASE !!!\n";
-			continue;
-		} else {
-		
-		// bulls and cows
-		int bulls = 0;
-		int cows = 0;
-		// inputted number as array
-		int numberArr[digit];
-		splitNumber(number[size], numberArr, digit);
-		// loop to find bulls and cows
-		for (int i = 0; i < digit; i++) {	// loop each digit of guessed number
-			int isBull = false;
-			int isCow = false;
-			for (int j = 0; j < digit; j++) {	// loop each digit of result
-				if (resultArr[j] == numberArr[i]) {	// if the same
-					if (i == j) {	// if it's the same place. it's cow
-						isCow = true;
-						break;
-					} else {		// if not the same place. it's bull
-						isBull = true;
-					}
-				}
-			}
-			// increse number of bulls or cows
-			if (isCow)			cows++;
-			else if (isBull)	bulls++;
-		}
-		// if 4 cows, RIGHT
-		if (cows == 4) {
-			size++;
-			break;
-		}
-		// output bulls and cows
-		cout << bulls << " bulls\n";
-		cout << cows << " cows\n";
-		size++;
-		}
-	}
-	
-	// output the winning message
-	cout << "---\n";
-	cout << "You guess it right\n";
-	cout << "The correct number is " << result << endl;;
-	cout << "These all the numbers you'd guessed.\n";
-	for (int i = 0; i < size; i++)
-		cout << number[i] << endl;
+using namespace std;
+
+void SplitNummer(int number, int array[], int size)
+{
+    for (; number != 0; number /= 10)
+        array[--size] = number%10;
+}
+int main()
+{
+    srand(time(NULL));
+    printf( "--- BULLS & COWS game ---\n\n");
+    int Guesses[100];
+    int size = 0;	/// guesses
+    /// randomizer
+    const short nummer = 4;
+    int result = rand()%9000 + 1000;
+    int resultArray[nummer];
+    int resultArrayCopy[nummer];
+    SplitNummer(result, resultArray, nummer);	/// resultaat gaat naar array
+    /// loop tot de input juist is
+    while(1)
+    {
+        /// input a number to guess
+        cout << "input a " << nummer << "-digits number : ";
+        cin >> Guesses[size];
+
+        if (Guesses[size] < 1000 || Guesses[size] > 9999)
+        {
+            cout << "!!! " << nummer << "-Bruh digits !!!\n";
+            continue;
+        }
+        else
+        {
+        /// bulls and cows
+        int bulls = 0;
+        int cows = 0;
+        /// inputted number as array
+        int numberArr[nummer];
+        SplitNummer(Guesses[size], numberArr, nummer);
+        /// loop find bulls and cows
+
+       memcpy(resultArrayCopy, resultArray, nummer*sizeof(int));
+        for (int i = 0; i < nummer; i++)
+        {
+            if (resultArrayCopy[i] == numberArr[i])
+            {    // if the same
+                cows++;
+                resultArrayCopy[i] = -1;
+                numberArr[i] = -1;
+            }
+        }
+
+        for (int i = 0; i < nummer; i++)
+        {
+            if ( numberArr[i] >= 0 )
+            {
+              for (int j = 0; j < nummer; j++) /// loop eelk nummer
+              {
+
+                 if ( resultArrayCopy[j] >= 0 && resultArrayCopy[j] == numberArr[i])
+                 {    // if the same
+                    bulls++;
+                    resultArrayCopy[j] = -1;
+                    numberArr[i] = -1;
+                    break;
+                }
+             }
+          }
+
+        }
+        /// if 4 cows = RIGHT
+        if (cows == 4) {
+            size++;
+            break;
+        }
+        /// weergeven van de aantallen
+        cout << bulls << " bulls\n";
+        cout << cows << " cows\n";
+        size++;
+        }
+    }
+    /// finale
+    cout << "---\n";
+    cout << "You guess it right\n";
+    cout << "The correct number is " << result << endl;;
+    cout << "These all the numbers you'd guessed.\n";
+    for (int i = 0; i < size; i++)
+        cout << Guesses[i] << endl;
+    return 0;
 }
